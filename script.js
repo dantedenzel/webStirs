b8c6da586458888a5768f7537a50698ba0555c85
danteburnett <danteburnett@gmail.com>
2022-07-12 08:09:17 -0700

    commit

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ function login(element) {
     }
 }
 
+function add() {
+    let counter = 0;
+    function plus() {counter += 1;}
+    plus();   
+    return counter;
+}
+
 function hide(el) {
     el.remove();
 }
